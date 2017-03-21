aeb0af9580f9298c63c1241296ba944e46aa24b5
Veronica Rispo <veronica_rispo@hotmail.it>
2017-03-21 23:22:39 +0100

    modifiche per ricezione piu messaggi dal socket e fix dei bug per i cambiamenti richiesti dai vari messaggi

--- src/MiriamGUI.java
+++ src/MiriamGUI.java
@@ -28,6 +28,7 @@ public class MiriamGUI {
 		
 		JLabel imgemail=new JLabel();
 		JLabel imgevent=new JLabel();
+		JLabel Mhello= new JLabel();
 
 		JFrame MGUI;
 		MGUI= new JFrame("Miriam GUI");
@@ -58,11 +59,6 @@ public class MiriamGUI {
 		Pan9.setBackground(Color.black);
 		Pan10.setBackground(Color.black);
 
-/*		Pan1.setPreferredSize(new Dimension(500,100));
-		Pan2.setPreferredSize(new Dimension(500,100));
-		Pan5.setPreferredSize(new Dimension(900,50));
-		Pan6.setPreferredSize(new Dimension(900,300));*/
-
 		//Initialization of the text area to show the email
 		MEmailPanel.setBackground(Color.black);
 		MEmailPanel.setForeground(Color.white);
@@ -87,6 +83,10 @@ public class MiriamGUI {
 		MCont.setBackground(Color.black);
 
 		Pan5.add(Lemailunread);
+		
+		Mhello.setForeground(Color.white);
+		Pan9.add(Mhello);
+		
 		Pan10.add(LeventN);
 		
 		//layout
@@ -184,161 +184,188 @@ public class MiriamGUI {
 		GBL.setConstraints(Pan6, BagC);
 		MCont.add(Pan6);
 		
-		MGUI.setVisible(false);
-
-		//JSON get
+		//DateTime panel setting
+		Pan1.add(MTimePanel=new TimePan());
+		MTimePanel.setFont(fTime);
+		MTimePanel.setForeground(Color.white);
+		Pan1.add(MDatePanel=new DatePan());
+		MDatePanel.setFont(fDate);
+		MDatePanel.setForeground(Color.white);
+	
+		GridBagLayout GBLpan1=new GridBagLayout();
+		Pan1.setLayout(GBLpan1);
+		GridBagConstraints BagCpan1=new GridBagConstraints();
+	
+		BagCpan1.fill=GridBagConstraints.BOTH;
+		BagCpan1.weightx=0;
+		BagCpan1.weighty=0;
+		BagCpan1.gridx=0;
+		BagCpan1.gridy=0;
+		GBLpan1.setConstraints(MTimePanel, BagCpan1);
+	
+		BagCpan1.fill=GridBagConstraints.BOTH;
+		BagCpan1.weightx=0;
+		BagCpan1.weighty=0;
+		BagCpan1.gridx=0;
+		BagCpan1.gridy=1;
+		GBLpan1.setConstraints(MDatePanel, BagCpan1);
 		
-		String jnew=sock.recvString();
-		sock.close();
-		System.out.println(jnew);
-		Gson g= new Gson();
-		/*String j="{\"weather\":\"08\",\"temperature\":18.71,\"unread\":2," +
-				"\"email_list\":[{\"name\":\"rossella carletti\",\"email\":\"carletti.rossella@gmail.com\",\"subject\":\"prova2\"}," +
-				"{\"name\":\"rossella carletti\",\"email\":\"carletti.rossella@gmail.com\",\"subject\":\"prova\"}],\"" +
-				"events\":[{\"name\":\"esame x\",\"time\":\"21 marzo 2017 11:00\"}," +
-				"{\"name\":\"esame y\",\"time\":\"27 marzo 2017 10:00\"}]" +
-				",\"user_id\":\"stupido chi legge\",\"command\":\"d\"}";*/
-		InfoGUI Minfo=(InfoGUI) g.fromJson(jnew, InfoGUI.class);
-		System.out.println(Minfo.toString());
+		//panel7 setting
+		ImageIcon imgemailico= new ImageIcon("./img/email.png","Email Icon");
+		Image xemail=imgemailico.getImage();
+		xemail=xemail.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgemailico.setImage(xemail);
+		imgemail= new JLabel(imgemailico);
+		imgemail.setMaximumSize(new Dimension(100,100));
+		Pan7.add(imgemail);
+		Pan7.setVisible(false);
+		
+		//panel8 setting
+		ImageIcon imgeventico= new ImageIcon("./img/calendario.jpg","Calendar Icon");
+		Image xevent=imgeventico.getImage();
+		xevent=xevent.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgeventico.setImage(xevent);
+		imgevent= new JLabel(imgeventico);
+		imgevent.setMaximumSize(new Dimension(100,100));
+		Pan8.add(imgevent);
+		Pan8.setVisible(false);
+		
+		//meteoicon
+		JLabel meteoimg;
+		meteoimg= new JLabel();
+		meteoimg.setMaximumSize(new Dimension(100,100));
+		
+		ImageIcon imgicon1= new ImageIcon("./img/sole.png","Sereno");
+		Image x=imgicon1.getImage();
+		x=x.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgicon1.setImage(x);
+		
+		ImageIcon imgicon2= new ImageIcon("./img/solenuvole.png","Nuvoloso");
+		Image x2=imgicon2.getImage();
+		x2=x2.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgicon2.setImage(x2);
+		
+		ImageIcon imgicon3= new ImageIcon("./img/pioggia.png","Pioggia");
+		Image x3=imgicon3.getImage();
+		x3=x3.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgicon3.setImage(x3);
+		
+		ImageIcon imgicon4= new ImageIcon("./img/temporale.png","Temporale");
+		Image x4=imgicon4.getImage();
+		x4=x4.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgicon4.setImage(x4);
+		
+		ImageIcon imgicon5= new ImageIcon("./img/neve.png","Neve");
+		Image x5=imgicon5.getImage();
+		x5=x5.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgicon5.setImage(x5);
+		
+		ImageIcon imgicon6= new ImageIcon("./img/nebbia.jpg","Nebbia");
+		Image x6=imgicon6.getImage();
+		x6=x6.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+		imgicon6.setImage(x6);
+		
+		Pan2.add(meteoimg);
+		Pan2.setVisible(false);
+		
+		while(true){	
+			//JSON get
+			String jnew=sock.recvString();
+		
+			System.out.println(jnew);
+			Gson g= new Gson();
 
-		if(Minfo.getCommand()=='d'){
-			//DateTime panel
-			Pan1.add(MTimePanel=new TimePan());
-			MTimePanel.setFont(fTime);
-			MTimePanel.setForeground(Color.white);
-			Pan1.add(MDatePanel=new DatePan());
-			MDatePanel.setFont(fDate);
-			MDatePanel.setForeground(Color.white);
-			
-			GridBagLayout GBLpan1=new GridBagLayout();
-			Pan1.setLayout(GBLpan1);
-			GridBagConstraints BagCpan1=new GridBagConstraints();
-			
-			BagCpan1.fill=GridBagConstraints.BOTH;
-			BagCpan1.weightx=0;
-			BagCpan1.weighty=0;
-			BagCpan1.gridx=0;
-			BagCpan1.gridy=0;
-			GBLpan1.setConstraints(MTimePanel, BagCpan1);
-			
-			BagCpan1.fill=GridBagConstraints.BOTH;
-			BagCpan1.weightx=0;
-			BagCpan1.weighty=0;
-			BagCpan1.gridx=0;
-			BagCpan1.gridy=1;
-			GBLpan1.setConstraints(MDatePanel, BagCpan1);
+			InfoGUI Minfo=(InfoGUI) g.fromJson(jnew, InfoGUI.class);
+			System.out.println(Minfo.toString());
 
-			//Email setting text
-			String tmpNU="Unread:"+Minfo.getUnread();
-			Lemailunread.setText(tmpNU);
-			
-			int ne=Minfo.getNumberOfEmail();
-			String[] emailtxt=new String[ne];
-			for(int i=0;i<ne;i++){
-				String[] parts=Minfo.getxSendertoString(i).split(",");
-				String tmpEmail="<html>"+parts[0]+"<br>"+parts[1]+"<br>"+parts[2]+"<br></html>";
-				emailtxt[i]=tmpEmail;			
-			}
-			MEmailPanel.setEmailtxt(emailtxt);
-			if(ne>0){
-				ImageIcon imgemailico= new ImageIcon("./img/email.png","Email Icon");
-				Image xemail=imgemailico.getImage();
-				xemail=xemail.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-				imgemailico.setImage(xemail);
-				imgemail= new JLabel(imgemailico);
-				imgemail.setMaximumSize(new Dimension(100,100));
-				Pan7.add(imgemail);
-			}
-			
-			//Event setting text			
-			int nev=Minfo.getNumberOfEvents();
-			String tmpNev="Event:"+nev;			
-			LeventN.setText(tmpNev);
-			
-			String[] eventtxt=new String[nev];
-			for(int i=0;i<nev;i++){
-				String[] parts=Minfo.getxEventtoString(i).split(",");
-				String tmpEvent="<html>"+parts[0]+"<br>"+parts[1]+"<br></html>";
-				eventtxt[i]=tmpEvent;			
-			}
-			MEventPanel.setEventtxt(eventtxt);
-			if(nev>0){
-				ImageIcon imgeventico= new ImageIcon("./img/calendario.jpg","Calendar Icon");
-				Image xevent=imgeventico.getImage();
-				xevent=xevent.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-				imgeventico.setImage(xevent);
-				imgevent= new JLabel(imgeventico);
-				imgevent.setMaximumSize(new Dimension(100,100));
-				Pan8.add(imgevent);
-			}
+			if(Minfo.getCommand()=='d'){
+				
+				Pan1.setVisible(true);
+				Pan6.setVisible(true);
+				//Email setting text
+				String tmpNU="Unread:"+Minfo.getUnread();
+				Lemailunread.setText(tmpNU);
+				Pan5.setVisible(true);
+				
+				int ne=Minfo.getNumberOfEmail();
+				String[] emailtxt=new String[ne];
+				for(int i=0;i<ne;i++){
+					String[] parts=Minfo.getxSendertoString(i).split(",");
+					String tmpEmail="<html>"+parts[0]+"<br>"+parts[1]+"<br>"+parts[2]+"<br></html>";
+					emailtxt[i]=tmpEmail;			
+				}
+				MEmailPanel.setEmailtxt(emailtxt);
+				Pan3.setVisible(true);
+				if(ne>0)
+					Pan7.setVisible(true);
+				
+				//Event setting text			
+				int nev=Minfo.getNumberOfEvents();
+				String tmpNev="Event:"+nev;			
+				LeventN.setText(tmpNev);
+				Pan10.setVisible(true);
+				
+				String[] eventtxt=new String[nev];
+				for(int i=0;i<nev;i++){
+					String[] parts=Minfo.getxEventtoString(i).split(",");
+					String tmpEvent="<html>"+parts[0]+"<br>"+parts[1]+"<br></html>";
+					eventtxt[i]=tmpEvent;			
+				}
+				MEventPanel.setEventtxt(eventtxt);
+				Pan4.setVisible(true);
+				if(nev>0)
+					Pan8.setVisible(true);
 
-			//hello panel
-			JLabel Mhello= new JLabel();
-			Date date=new Date();
-			int h=date.getHours();
-			String Muserinfo;
-			if(h<=12)
-				Muserinfo="<html>Good Morning "+Minfo.getClientinfo()+"</html>";
-			else
-				Muserinfo="<html>Good Evening "+Minfo.getClientinfo()+"</html>";
-			Mhello.setText(Muserinfo);
-			Mhello.setForeground(Color.white);
-			Pan9.add(Mhello);
-
-			//meteo panel
-			int currW=Integer.parseInt(Minfo.getWeather());
-			System.out.println(currW);
-			JLabel img;
-			switch(currW){
-				case 1: ImageIcon imgicon1= new ImageIcon("./img/sole.png","Sereno");
-						Image x=imgicon1.getImage();
-						x=x.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-						imgicon1.setImage(x);
-						img= new JLabel(imgicon1);
-						img.setMaximumSize(new Dimension(100,100));
-					break;
-				case 2:
-				case 3:
-				case 4: ImageIcon imgicon2= new ImageIcon("./img/solenuvole.png","Nuvoloso");
-						Image x2=imgicon2.getImage();
-						x2=x2.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-						imgicon2.setImage(x2);
-						img= new JLabel(imgicon2);
-						img.setMaximumSize(new Dimension(100,100));
-					break;
-				case 5:
-				case 6: ImageIcon imgicon3= new ImageIcon("./img/pioggia.png","Pioggia");
-						Image x3=imgicon3.getImage();
-						x3=x3.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-						imgicon3.setImage(x3);
-						img= new JLabel(imgicon3);
-						img.setMaximumSize(new Dimension(100,100));
-					break;
-				case 7: ImageIcon imgicon4= new ImageIcon("./img/temporale.png","Temporale");
-						Image x4=imgicon4.getImage();
-						x4=x4.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-						imgicon4.setImage(x4);
-						img= new JLabel(imgicon4);
-						img.setMaximumSize(new Dimension(100,100));
-					break;
-				case 8: ImageIcon imgicon5= new ImageIcon("./img/neve.png","Neve");
-						Image x5=imgicon5.getImage();
-						x5=x5.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-						imgicon5.setImage(x5);
-						img= new JLabel(imgicon5);
-						img.setMaximumSize(new Dimension(100,100));
-					break;
-				case 9: ImageIcon imgicon6= new ImageIcon("./img/nebbia.jpg","Nebbia");
-						Image x6=imgicon6.getImage();
-						x6=x6.getScaledInstance(100, 100,Image.SCALE_DEFAULT);
-						imgicon6.setImage(x6);
-						img= new JLabel(imgicon6);
-						img.setMaximumSize(new Dimension(100,100));
-					break;
-				default: img=new JLabel();
+				//hello panel
+				
+				Date date=new Date();
+				int h=date.getHours();
+				String Muserinfo;
+				if(h<=12)
+					Muserinfo="<html>Good Morning "+Minfo.getClientinfo()+"</html>";
+				else
+					Muserinfo="<html>Good Evening "+Minfo.getClientinfo()+"</html>";
+				Mhello.setText(Muserinfo);
+				Pan9.setVisible(true);
+	
+				//meteo panel
+				int currW=Integer.parseInt(Minfo.getWeather());
+				System.out.println(currW);
+				switch(currW){
+					case 1: meteoimg.setIcon(imgicon1);
+						break;
+					case 2:
+					case 3:
+					case 4: meteoimg.setIcon(imgicon2);
+						break;
+					case 5:
+					case 6: meteoimg.setIcon(imgicon3);
+						break;
+					case 7: meteoimg.setIcon(imgicon4);
+						break;
+					case 8: meteoimg.setIcon(imgicon5);
+						break;
+					case 9: meteoimg.setIcon(imgicon6);
+						break;
+					default:
+				}
+				Pan2.setVisible(true);
+				
+				MGUI.setVisible(true);
 			}
-			Pan2.add(img);
-			MGUI.setVisible(true);
+			else{
+				Pan1.setVisible(false);
+				Pan2.setVisible(false);
+				Pan3.setVisible(false);
+				Pan4.setVisible(false);
+				Pan4.setVisible(false);
+				Pan5.setVisible(false);
+				Pan6.setVisible(false);
+				Pan7.setVisible(false);
+				Pan8.setVisible(false);
+				Pan9.setVisible(false);
+				Pan10.setVisible(false);
+			}
 		}
 	}
 
--- src/Prova.java
+++ src/Prova.java
@@ -11,7 +11,8 @@ public class Prova {
 				"\"email_list\":[{\"name\":\"rossella carletti\",\"email\":\"carletti.rossella@gmail.com\",\"subject\":\"prova2\"}," +
 				"{\"name\":\"veronica rispo\",\"email\":\"veronica.rispo@gmail.com\",\"subject\":\"fottiti\"}],\"" +
 				"events\":[{\"name\":\"esame x\",\"time\":\"21 marzo 2017 11:00\"}," +
-				"{\"name\":\"esame y\",\"time\":\"27 marzo 2017 10:00\"}]" +
+				"{\"name\":\"esame y\",\"time\":\"27 marzo 2017 10:00\"},"
+				+ "{\"name\":\"bla bla bla y\",\"time\":\"27 marzo 2017 10:00\"}]" +
 				",\"user_id\":\"stupido chi legge\",\"command\":\"d\"}";
 
         sock.send(j);
